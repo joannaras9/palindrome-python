1ca825d6234cb74dd9136ff73f50164fc829d5ce
joannaras9 <100958925+joannaras9@users.noreply.github.com>
2022-07-18 13:46:44 +0200

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+class Palindrome:
+    def __init__(self,word):
+        self.word = word
+
+def reverse(self, word):
+    reverse.list = []
+    for c in word[:: -1]:
+        reverse.list.append(c)
+        print(reverse.list)
+    reverse_word = ''.join(reverse.list)
+    print (reverse_word)
+
+def isPalindrome(self, word):
+    if word == word[:: -1]:
+            print True
+    else:
+        print False
